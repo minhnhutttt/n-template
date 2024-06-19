93f5af9434da189277e3698845c68d62a1ff944a
rabi-minhnhut <nhut@rabi.co.jp>
2024-06-19 18:13:49 +0700

    a

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,90 @@
-import { buttonVariants } from "@/lib/variants/buttonVariants";
-
+"use client";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+const data = [
+  {
+    image: "/assets/images/sauri_009.jpg",
+    title: "イベント運営会社",
+    text: (
+      <>
+        フェスをデジタルでさらに楽しむ！
+        <br />
+        NFTクーポンでフェスティバルの特典や限定アイテムを提供！
+      </>
+    ),
+  },
+  {
+    image: "/assets/images/sauri_009.jpg",
+    title: "イベント運営会社",
+    text: (
+      <>
+        フェスをデジタルでさらに楽しむ！
+        <br />
+        NFTクーポンでフェスティバルの特典や限定アイテムを提供！
+      </>
+    ),
+  },
+  {
+    image: "/assets/images/sauri_009.jpg",
+    title: "イベント運営会社",
+    text: (
+      <>
+        フェスをデジタルでさらに楽しむ！
+        <br />
+        NFTクーポンでフェスティバルの特典や限定アイテムを提供！
+      </>
+    ),
+  },
+  {
+    image: "/assets/images/sauri_009.jpg",
+    title: "イベント運営会社",
+    text: (
+      <>
+        フェスをデジタルでさらに楽しむ！
+        <br />
+        NFTクーポンでフェスティバルの特典や限定アイテムを提供！
+      </>
+    ),
+  },
+  {
+    image: "/assets/images/sauri_009.jpg",
+    title: "イベント運営会社",
+    text: (
+      <>
+        フェスをデジタルでさらに楽しむ！
+        <br />
+        NFTクーポンでフェスティバルの特典や限定アイテムを提供！
+      </>
+    ),
+  },
+];
 export default function Home() {
+  var settings = {
+    className: "center",
+    centerMode: true,
+    infinite: true,
+    slidesToShow: 3,
+    speed: 500,
+    variableWidth: true,
+  };
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between bg-red-400 p-24">
-      <div className="mx-auto h-[1000px] w-[1000px] bg-blue-800">
-        <button className={buttonVariants({ type: "primary", full: true })}>
-          キャンセル
-        </button>
+    <main className="">
+      <p className="p-5 text-center">Deck ID : 0x.....</p>
+      <div className="js-slider overflow-hidden">
+        <Slider {...settings}>
+          {data.map((item, index) => (
+            <div key={index} className="px-3">
+              <figure className="h-[500px]">
+                <img
+                  className="size-full object-cover"
+                  src={item.image}
+                  alt=""
+                />
+              </figure>
+            </div>
+          ))}
+        </Slider>
       </div>
     </main>
   );
