1ce25081624b63ce0a591b37c7a54011c09857a3
rabi-minhnhut <nhut@rabi.co.jp>
2024-06-27 09:31:33 +0700

    a

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,6 @@
-import { buttonVariants } from "@/lib/variants/buttonVariants";
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between bg-red-400 p-24">
-      <div className="mx-auto h-[1000px] w-[1000px] bg-blue-800">
-        <button className={buttonVariants({ type: "primary", full: true })}>
-          キャンセル
-        </button>
-      </div>
+    <main>
     </main>
   );
 }
