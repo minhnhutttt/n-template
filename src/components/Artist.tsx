36222a33ee8848a8dc7c4b7dda0d276e2b27fd9e
rabi-minhnhut <nhut@rabi.co.jp>
2024-07-01 22:22:11 +0700

    a

--- src/components/Artist.tsx
+++ src/components/Artist.tsx
@@ -1,7 +1,7 @@
 const Artist = () => {
     return (
       <div className=" relative">
-        <a href="/" className="fixed block top-0 right-0 max-xl:w-14">
+        <a href="/" className="fixed block top-0 right-0 max-xl:w-14 z-10 hover:opacity-75 duration-150">
         <img src="/assets/images/contact.png" alt="" /></a>
           <div className="md:mt-[64px] absolute top-0 w-full px-6 pr-10 xl:pr-[115px]">
               <img className="" src="/assets/images/artist.png" alt="" />
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,9 +11,9 @@ const Footer = () => {
             <a href="/privacy-policy" className="hover:opacity-75 duration-150">プライバシーポリシー</a>
         </div>
         <div className="flex justify-center items-cenrer gap-16 pt-3 pb-14">
-            <a href="/"><img src="/assets/images/logo-tiktok.png" alt="" /></a>
-            <a href="/"><img src="/assets/images/logo-instagram.png" alt="" /></a>
-            <a href="/"><img src="/assets/images/logo-line.png" alt="" /></a>
+            <a href="/" className="duration-150 hover:opacity-75"><img src="/assets/images/logo-tiktok.png" alt="" /></a>
+            <a href="https://www.instagram.com/toplive_tiktok/" target="_blank" className="duration-150 hover:opacity-75"><img src="/assets/images/logo-instagram.png" alt="" /></a>
+            <a href="https://lin.ee/RFM7pae" target="_blank" className="duration-150 hover:opacity-75"><img src="/assets/images/logo-line.png" alt="" /></a>
         </div>
         <p className="text-center md:text-[16px] text-[14px] text-white font-light">copyright © 2024 TOP LIVE</p>
     </footer>
